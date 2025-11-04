c30648f51087b989c974f8f4ab4a7407d69da849
nsamuolyte <nsamuolyte@icloud.com>
2025-11-04 09:55:51 +0200

    Klasės ir jų apsirašymas 1

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.budgetboltfood {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires static lombok;
+
+    opens com.example.budgetboltfood to javafx.fxml;
+    exports com.example.budgetboltfood;
+}
